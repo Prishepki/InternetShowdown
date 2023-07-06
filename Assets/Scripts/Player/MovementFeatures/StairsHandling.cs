af0f76c7eaa518da0856f2a5ed538730215b1b19
zstardustttt <blastik2281337@gmail.com>
2023-07-07 00:14:30 +0700

    war crimes

---
+++ Assets/Scripts/Player/MovementFeatures/StairsHandling.cs
@@ -0,0 +1,56 @@
+using Mirror;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class StairsHandling : NetworkBehaviour
+{
+    [Header("Objects")]
+    [SerializeField] private Transform _orientation;
+    [SerializeField] private Rigidbody _rb;
+
+    [Header("Settings")]
+    [SerializeField] private LayerMask _targetLayers;
+    [SerializeField] private float _minStepHeight = 0.05f;
+    [SerializeField] private float _maxStepHeight = 0.3f;
+
+    [Space(9)]
+
+    [SerializeField] private float _stepSpeed = 0.1f;
+
+    private Vector3 _stepMinOrigin
+    {
+        get => transform.position + Vector3.up * _minStepHeight;
+    }
+
+    private Vector3 _stepMaxOrigin
+    {
+        get => transform.position + Vector3.up * _maxStepHeight;
+    }
+
+    private void OnValidate()
+    {
+        TryGetComponent<Rigidbody>(out _rb);
+    }
+
+    private void FixedUpdate()
+    {
+        if (!isLocalPlayer) return;
+
+        bool IsMinStep = Physics.Raycast(_stepMinOrigin, _orientation.forward, 1.25f, _targetLayers);
+        bool IsMaxStep = Physics.Raycast(_stepMaxOrigin, _orientation.forward, 1.25f, _targetLayers);
+
+        if (IsMinStep && !IsMaxStep)
+        {
+            _rb.velocity = new Vector3(_rb.velocity.x, _stepSpeed, _rb.velocity.z);
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = ColorISH.Green;
+        Gizmos.DrawLine(_stepMinOrigin, _stepMinOrigin + _orientation.forward * 1.25f);
+
+        Gizmos.color = ColorISH.Red;
+        Gizmos.DrawLine(_stepMaxOrigin, _stepMaxOrigin + _orientation.forward * 1.25f);
+    }
+}
--- Assets/Scripts/Player/NetworkPlayer.cs
+++ Assets/Scripts/Player/NetworkPlayer.cs
@@ -62,12 +62,6 @@ public class NetworkPlayer : NetworkBehaviour
     [Header("Ground Dashing Control")]
     [SerializeField, Tooltip("Сила рывка вниз"), Min(0)] private float _groundDashForce = 5f;
 
-    // никита я хуй знает куда поместить это в списке свойств я тут запутался
-    [SerializeField] private float _maxStepHeight = 0.3f;
-    [SerializeField] private float _stepSmooth = 2f;
-    private Vector3 _stepUp;
-    private Vector3 _stepDown;
-
     [Header("Property Checking")]
     [SerializeField] private LayerMask _mapLayers;
 
@@ -358,9 +352,6 @@ public class NetworkPlayer : NetworkBehaviour
 
         PlayerCurrentStats.Singleton.ResetStats();
         PlayerMutationStats.Singleton.ResetStats();
-
-        _stepUp = _stepDown = transform.position;
-        _stepUp.y += _maxStepHeight;
     }
 
     private void SetupPlayerAndGameObject()
@@ -688,16 +679,6 @@ public class NetworkPlayer : NetworkBehaviour
             Vector3.Angle(Vector3.up, _slopeNormal) * 1.75f
         );
 
-        bool stepTestDown = Physics.Raycast(_stepDown, _orientation.transform.forward, 2f);
-        bool stepTestUp = Physics.Raycast(_stepUp, _orientation.transform.forward, 2f);
-
-        if (stepTestDown && !stepTestUp)
-        {
-            Vector3 pos = _rb.transform.position;
-            pos.y += _stepSmooth * Time.fixedDeltaTime;
-            _rb.MovePosition(pos);
-        }
-
         Vector3 targetDirection = IsSloped() ? Vector3.ProjectOnPlane(_playerDirection, _slopeNormal) : _playerDirection;
 
         PlayerCurrentStats.Singleton.Speed = (_startSpeed + _accel + _bhop + angleBoost);
@@ -800,7 +781,6 @@ public class NetworkPlayer : NetworkBehaviour
         Gizmos.color = Color.green;
 
         Gizmos.DrawWireCube(_groundChecking.center, _groundChecking.extends);
-        Gizmos.DrawLine(_stepUp, _orientation.forward * 2f);
     }
 }
 
